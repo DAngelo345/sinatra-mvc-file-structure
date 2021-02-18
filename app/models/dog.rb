47ace8a3cf9517d0097fc822702b990c0c9cd3a2
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-02-17 19:28:08 -0500

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+
+   
+
+    @@all = []
+
+
+    def initialize(name, breed, age)
+        @dog = dog
+    end
+
+    def self.all
+        Dog.all
+
+    end
+
+    def save 
+       saved = @@all << Dog.new(name, breed, age)
+        
+    end
+end
